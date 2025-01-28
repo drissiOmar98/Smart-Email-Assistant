5d92264d3c91edecac597382627098c52e17e4e4
DrissiOmar <omar.drissi@esprit.tn>
2025-01-28 13:30:28 +0100

    Add email generation feature with dynamic prompt building and API integration

---
+++ Backend/Smart-Email-Assistant/src/main/java/dev/omar/Smart_Email_Assistant/EmailRequest.java
@@ -0,0 +1,25 @@
+package dev.omar.Smart_Email_Assistant;
+
+
+
+
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+}
