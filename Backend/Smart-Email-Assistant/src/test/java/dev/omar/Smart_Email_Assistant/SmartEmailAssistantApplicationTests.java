15232f8dc65ecf1a3d3636b6017deded7545e9ce
DrissiOmar <omar.drissi@esprit.tn>
2025-01-28 13:15:21 +0100

    Initial commit: Added Backend files and .gitignore

---
+++ Backend/Smart-Email-Assistant/src/test/java/dev/omar/Smart_Email_Assistant/SmartEmailAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.omar.Smart_Email_Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
